21c0d63dda8f550af8727b8090d03383f8a19e16
Alejandro Montes <alejandro.montes.g@gmail.com>
2025-12-22 14:49:54 +0100

    🎉 Basic setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Invest AI Assist
+
+An open-source AI investing assistant that evaluates a user’s risk tolerance, financial situation, and existing portfolio to provide personalized monthly investment recommendations. Designed to support long-term decision-making with clear, structured guidance
+
+## License
+
+This project is licensed under the Creative Commons
+Attribution–NonCommercial 4.0 International License (CC BY-NC 4.0).
+
+Commercial use is not permitted without explicit permission.
---
+++ eslint.config.ts
@@ -0,0 +1,35 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettier,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      quotes: [
+        'error',
+        'single',
+        { avoidEscape: true, allowTemplateLiterals: true },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'all',
+        },
+      ],
+    },
+  },
+]);
